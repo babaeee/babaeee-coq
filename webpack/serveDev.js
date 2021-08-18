3ad8ccb265edf640934886f3d10c56c4280d843b
hamidreza kalbasi <hamidrezakalbasi@protonmail.com>
2021-08-18 13:06:02 +0430

    use local http-server with npx

--- webpack/serveDev.js
+++ webpack/serveDev.js
@@ -2,7 +2,8 @@ import { spawn } from "child_process";
 import { buildFolder } from "../paths.js";
 
 export const serveDev = (p = '32664') => {
-  const server = spawn('http-server', [
+  const server = spawn('npx', [
+    'http-server',
     buildFolder,
     '--port',
     p,
